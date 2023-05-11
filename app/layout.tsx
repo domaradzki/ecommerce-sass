96f18e65dfea59743bfa959b035a10f8c505e5fe
Domaradzki Grzegorz <domaradzki.g@gmail.com>
2023-05-11 07:14:40 +0200

    migration to app router

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: 'Home',
+  description: 'Welcome to Next.js',
+};
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pl">
+      <body>{children}</body>
+    </html>
+  );
+}
